e65ee1c0c37b7f66acd6d915f8818e48a7905bef
tensho1026 <tensho1026@icloud.com>
2026-05-08 02:44:44 +0900

    fix:pass

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/users/entities/user.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
